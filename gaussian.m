f4d78cc4d1276139ab1fde992ad688a79c31847f
Martin Kersner <m.kersner@gmail.com>
2014-07-23 23:49:21 +0900

    cant upload all file of gaussian.m :/

--- gaussian.m
+++ gaussian.m
@@ -4,8 +4,13 @@
 %%
 %% Computes average of neighbor pixels with GAUSSIAN kernel.
 %% I input image
-%% S size of the box
+%% S size of the kernel
 %% F noise supressed image
-function F = gaussian(I, S)
+function F = gaussian(I, S, sigma)
+  b = int16(S/2); % boundary
+  [x y] = meshgrid(-b:b);
+  g = exp(-x.^2/(2*sigma^2) - y.^2/(2*sigma^2));
+  g = g./sum(g(:));
 
+  F = uint8(conv2(I, g, 'same'));
 endfunction
