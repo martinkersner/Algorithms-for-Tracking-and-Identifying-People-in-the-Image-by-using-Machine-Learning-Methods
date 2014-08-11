52a3fdfbf574c91ce872399874876045e9d39164
Martin Kersner <m.kersner@gmail.com>
2014-08-11 19:19:28 +0200

    revised version of histogram of normals

--- histogramOfNormals.m
+++ histogramOfNormals.m
@@ -2,13 +2,12 @@
 %% Martin Kersner, m.kersner@gmail.com
 %% 08-11-2014
 %%
-%% Computes feature vector from the tangent and bitangent, which are cross 
+%% Computes Histogram of Normals. 
 %% producted, of given image.
 %%
 %% F     feature vector
 %% I     examined image
 %% B     coordinated of blocks of image
-%%       height of the block has to be 3 due to cross product
 %% BIN   number of bins of histogram
 function F = histogramOfNormals(I, B, BIN)
   so1 = [1; 2; 1];
@@ -20,17 +19,23 @@ function F = histogramOfNormals(I, B, BIN)
   so_y = (so1 * so2(end:-1:1))';
 
   % compute tangent and bitangent
-  t = conv2(double(I), so_x, 'same');
-  b = conv2(double(I), so_y, 'same');
+  d_x = double(conv2(double(I), so_x, 'same'));
+  d_y = double(conv2(double(I), so_y, 'same'));
+
+  % azimut angle
+  phi = atan(d_x ./ d_y);
+  
+  % zenit angle
+  theta = atan(sqrt(d_x.^2 + d_y.^2));
 
   % split to blocks
   for i = 1:2:size(B)
-    tb = t(B(i,1):B(i+1,1), B(i,2):B(i+1,2));
-    bb = b(B(i,1):B(i+1,1), B(i,2):B(i+1,2));
+    phi_b = phi(B(i,1):B(i+1,1), B(i,2):B(i+1,2));
+    theta_b = theta(B(i,1):B(i+1,1), B(i,2):B(i+1,2));
 
-    % cross product
-    c = cross(tb, bb);
-    F = [F, hist(c(:), BIN)];
+    % 2D histogram
+    [h, hi] = hist3([phi_b(:), theta_b(:)], [BIN BIN]);
+
+    F = [F, hist(h(:), BIN)];
   endfor
 endfunction
-
