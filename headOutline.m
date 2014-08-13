41e3d0a4274067ccbf1a43b0cf03d8c78437257e
Martin Kersner <m.kersner@gmail.com>
2014-08-13 11:50:41 +0200

    added searching for centroid of head

--- headOutline.m
+++ headOutline.m
@@ -3,13 +3,17 @@
 %% 08-12-2014
 %%
 %% Finds a head in the normalized image, draws an outline around it
-%% and return coordinates of head center.
+%% and return coordinates of head center. We assume that given shapes have
+%% already been preselected. We do not deal with issues such as control of size
+%% of shape.
 %%
 %% O   output image
+%% X   X-axis coordinate (collumns)
+%% Y   Y-axis coordinate (rows)
 %% I   examined image
 %% B   number of bins
 %% P   position of bin with decision threshold
-function O = headOutline(I, B=20, P=2)
+function [O, X, Y] = headOutline(I, B=20, P=2)
   [h, hi] = hist(I, B);
   v_h = hi(end:-1:1);
   t = v_h(P);
@@ -19,6 +23,15 @@ function O = headOutline(I, B=20, P=2)
 
   % finds edges of head shape
   e = edge(h, 'Canny');
+
+  % binary mask
+  bm = zeros(size(I));
+  bm(h > 0) = 1;
+
+  % centroid coordinates of 'head'
+  c = regionprops(bm, 'Centroid');
+  X = uint16(c.Centroid(1));
+  Y = uint16(c.Centroid(2));
 
   % draw red edges around head in input image
   rgb_e = 255 * e;
